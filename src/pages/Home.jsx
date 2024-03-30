d05d31950bd418c9ee854199237e7185cb995339
Rikcher <nick.richardson.4884@gmail.com>
2024-03-30 10:23:40 +0700

    trying to organize pubplic folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ function Home() {
   return (
     <>
     <div className="background">
-      <img id='forest' src="/HomePageForestBG.svg" alt="" />
-      <img id='beam' src="/HomePageBeamBG.svg" alt="" />
-      <img id='items' src="/HomePageItemsInBeamBG.svg" alt="" />    
-      <img id='ufo' src="/HomePageUfoBG.svg" alt="" />    
-      <img id='title' src="/HomePageHeroTitle.svg" alt="" />    
-      <img id='headset' src="/HomePageHeadsetBG.svg" alt="" />    
-      <img id='keyboard' src="/HomePageKeyboardBG.svg" alt="" />    
+      <img draggable="false" id='forest' src="/home-page/HomePageForestBG.svg" alt="" />
+      <img draggable="false" id='beam' src="/HomePageBeamBG.svg" alt="" />
+      <img draggable="false" id='items' src="/HomePageItemsInBeamBG.svg" alt="" />    
+      <img draggable="false" id='ufo' src="/HomePageUfoBG.svg" alt="" />    
+      <img draggable="false" id='title' src="/HomePageHeroTitle.svg" alt="" />    
+      <img draggable="false" id='headset' src="/HomePageHeadsetBG.svg" alt="" />    
+      <img draggable="false" id='keyboard' src="/HomePageKeyboardBG.svg" alt="" />    
     </div>
     <Link id='ctaButton' to="#">Shop Now</Link>
     </>
