1a6be9a87191570db837462c751f433d75e2f16d
Rikcher <nick.richardson.4884@gmail.com>
2024-04-25 09:02:35 +0700

    Trying to clear users cart after successfull payment

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,7 +1,32 @@
 import { Link } from 'react-router-dom';
+import { auth } from '/src/firebase-config.jsx';
+import { getDatabase, ref, onValue, remove, update } from 'firebase/database';
+import { useEffect, useState } from 'react';
 import '/src/styles/css/Success.css'
 
 const Success = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setUser(user); // Update user state
+        });
+
+        // Clean up the observer when the component unmounts
+        return () => unsubscribe();
+    }, []);
+    
+    // Clear cart after checkout
+    useEffect(() => {
+        if (user) {
+            const database = getDatabase();
+            const deleteRef = ref(database, `carts/${user.uid}/`);
+            remove(deleteRef)
+                .then(() => console.log('Cart cleared successfully'))
+                .catch((error) => console.error('Error clearing cart:', error));
+        }
+    }, [user]); // Add user as a dependency to re-run this effect when user changes
+
     return ( 
         <div className="successPageWrapper">
             <div className='successIcon'></div>
